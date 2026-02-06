2c5523798b6f4b5b886cc20d1ad20e562748d9cd
AlwinNg <alwinng420@gmail.com>
2026-02-06 09:26:48 -0500

    c

--- src/main/java/com/stuypulse/robot/subsystems/spindexer/SpindexerImpl.java
+++ src/main/java/com/stuypulse/robot/subsystems/spindexer/SpindexerImpl.java
@@ -2,8 +2,14 @@ package com.stuypulse.robot.subsystems.spindexer;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.MotorAlignmentValue;
+import com.stuypulse.robot.constants.Field;
 import com.stuypulse.robot.constants.Motors;
 import com.stuypulse.robot.constants.Ports;
+import com.stuypulse.robot.subsystems.swerve.CommandSwerveDrivetrain;
+import com.stuypulse.robot.util.SpindexerInterpolation;
+
+import edu.wpi.first.math.geometry.Translation2d;
+
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.VoltageOut;
 
@@ -21,7 +27,10 @@ public class SpindexerImpl extends Spindexer {
     }
 
     public double getVoltageBasedOnDistance() {
-        return 2; //return distanceFromHub * dynamic
+        Translation2d hub = Field.getHubCenterPose().getTranslation();
+        Translation2d rob = CommandSwerveDrivetrain.getInstance().getPose().getTranslation();
+        double distance = hub.getDistance(rob);
+        return SpindexerInterpolation.getVoltage(distance); 
     }
 
     @Override
