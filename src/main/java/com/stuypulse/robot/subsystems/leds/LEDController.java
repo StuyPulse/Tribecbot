1e86a9864d97a4bbe73e95e7cb4aa9bf6b160105
SP-COMPuter <se@stuypulse.com>
2026-05-12 12:34:43 -0400

    feat: began writing the logic. tested through tuner and noticed that on tuner (need to see if this is actually the case) when you change the indices of the start and end, it doesnt update on the candle unless you disable. Started adding logic for that.

--- src/main/java/com/stuypulse/robot/subsystems/leds/LEDController.java
+++ src/main/java/com/stuypulse/robot/subsystems/leds/LEDController.java
@@ -7,6 +7,17 @@
 
 package com.stuypulse.robot.subsystems.leds;
 
+import com.ctre.phoenix6.configs.CANdleConfiguration;
+import com.ctre.phoenix6.configs.CANdleFeaturesConfigs;
+import com.ctre.phoenix6.configs.CustomParamsConfigs;
+import com.ctre.phoenix6.configs.LEDConfigs;
+import com.ctre.phoenix6.controls.EmptyAnimation;
+import com.ctre.phoenix6.controls.SolidColor;
+import com.ctre.phoenix6.hardware.CANdle;
+import com.ctre.phoenix6.signals.LossOfSignalBehaviorValue;
+import com.ctre.phoenix6.signals.RGBWColor;
+import com.ctre.phoenix6.signals.StatusLedWhenActiveValue;
+import com.ctre.phoenix6.signals.StripTypeValue;
 import com.stuypulse.robot.RobotContainer;
 import com.stuypulse.robot.constants.Ports;
 import com.stuypulse.robot.constants.Settings;
@@ -15,6 +26,7 @@ import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LEDController extends SubsystemBase {
@@ -32,33 +44,74 @@ public class LEDController extends SubsystemBase {
     private AddressableLED leds;
     private AddressableLEDBuffer ledsBuffer;
 
+    private final CANdle candle;
+    private CANdleConfiguration candleConfigs;
+    private RGBWColor candleColor;
+
+    private int startingIndex = 0;
+    private int endIndex = 7;
+    private boolean indicesChanged = false;
+
     private final LEDPattern defaultPattern = LEDPattern.kOff;
 
-    protected LEDController(int port, int length) {
-        leds = new AddressableLED(port);
-        ledsBuffer = new AddressableLEDBuffer(length);
+    protected LEDController(int ledPort, int ledLength) { // TODO: add brightness of CANdle to the constructor
+        leds = new AddressableLED(ledPort);
+        ledsBuffer = new AddressableLEDBuffer(ledLength);
 
-        leds.setLength(length);
+        leds.setLength(ledLength);
         leds.setData(ledsBuffer);
         leds.start();
 
         applyPattern(defaultPattern);
 
         SmartDashboard.putData(instance);
+
+        candle = new CANdle(null, Ports.CANIVORE); // TODO: update ports value
+
+        candleConfigs = new CANdleConfiguration()
+                .withLED(
+                        new LEDConfigs()
+                                .withBrightnessScalar(0.7)
+                                .withStripType(StripTypeValue.RGB)
+                                .withLossOfSignalBehavior(LossOfSignalBehaviorValue.DisableLEDs))
+                                
+                .withCANdleFeatures(
+                        new CANdleFeaturesConfigs().withStatusLedWhenActive(StatusLedWhenActiveValue.Disabled));
+                
+        candle.getConfigurator().apply(candleConfigs);
     }
 
     public void applyPattern(LEDPattern pattern) {
         pattern.applyTo(ledsBuffer);
     }
 
+    public void changeCandleIndices(int start, int end) {
+        this.startingIndex = start;         
+        this.endIndex = end;
+        
+        indicesChanged = true;
+    }
+
     public void periodicAfterScheduler() {
         if (RobotContainer.EnabledSubsystems.LEDS.get()) {
             // leds.start();
             leds.setData(ledsBuffer);
-        }
-        else {
+
+            candleColor = new RGBWColor(
+                    new Color8Bit(
+                            ledsBuffer.getLED(1).toHexString()));
+
+            if (indicesChanged) {
+                //TODO: add logic that turns the candle on and off. Use output current amperage (if it is 0.01 A or lower) to determine if it fully turned off.
+            }
+            
+            candle.setControl(new SolidColor(startingIndex, endIndex).withColor(candleColor));
+
+        } else {
             LEDPattern.kOff.applyTo(ledsBuffer);
             leds.setData(ledsBuffer);
+
+            //candle.setControl(new EmptyAnimation(0));
         }
         // SmartDashboard.putString("Leds/Color", ledsBuffer.getLED(1).toString());
     }
