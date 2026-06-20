a2b246bd8fba15a86f0dededc183965873fd6963
DanTheMan95 <81121522+Danx3mer@users.noreply.github.com>
2026-06-20 10:41:56 -0400

    feat: reset heading to zero when shooting up against hub

--- src/main/java/com/stuypulse/robot/commands/swerve/SwerveResetPoseKBShot.java
+++ src/main/java/com/stuypulse/robot/commands/swerve/SwerveResetPoseKBShot.java
@@ -4,6 +4,7 @@ import com.stuypulse.robot.constants.Field;
 import com.stuypulse.robot.subsystems.swerve.CommandSwerveDrivetrain;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 
 public class SwerveResetPoseKBShot extends SwerveResetPose {
@@ -11,6 +12,6 @@ public class SwerveResetPoseKBShot extends SwerveResetPose {
         super(new Pose2d(
             Field.HUB_CENTER.getX() - Field.HUB_RADIUS - Units.inchesToMeters(23.5), 
             Field.WIDTH / 2 - Units.inchesToMeters(6.5), 
-            CommandSwerveDrivetrain.getInstance().getPose().getRotation()));
+            Rotation2d.kZero));     
     } 
 }
