d0eed35092bdc141842548bc2ec203a34e969780
Alex Wang <148587179+alex-wanger@users.noreply.github.com>
2026-09-14 16:53:20 -0400

    merge CC to main (#34)
    * Feat: Chezy Bump Auton Draft
    
    * feat: auton testing
    
    * feat: auton changes abrupt
    
    * PATH ADJUST: change starting pose and rotational target
    
    * AUTO FIX: increase bump speed and wait time
    
    * PATH ADJUST: extend bump end point
    
    * PATH ADJUST: fix path to depo
    
    * AUTO + PATH ADJUST: wait time down and depo path fix
    
    * AUTO ADJUST: make intake keep digesting
    
    * AUTO ADJUST: change contstraints
    
    * feat (auton-chezy depot): add back and forth paths, clean auton code, infinite auto digest, infinite back and forth
    
    * fix (auton-chezy depot): fix paths such that the intake is centered on the curve for 1st pass
    
    * feat (auton-chezy depot): don't digest when shooting preloads
    
    * AUTO: finish depo start right side
    
    * FEAT: log active path
    
    * feat: restore follow autons, modify right bump deceleration path and first pass speed
    
    * feat: make a duplicate champs auton for chezy for quick modification
    
    * fix (auton-chezy non depot): make starting constraint zone start earlier
    
    * feat: modify right chezy bump auton, wait for less on pose stabilization (1.3->0.5s)
    
    * feat (auton-chezy non depot): infinite auto digest while shooting
    
    ---------
    
    Co-authored-by: Ryan Bergman <rbergman80@stuy.edu>
    Co-authored-by: DanTheMan95 <81121522+Danx3mer@users.noreply.github.com>
    Co-authored-by: SP-COMPuter <se@stuypulse.com>
    Co-authored-by: Danx3mer <dashingdaniel05@gmail.com>

--- src/main/java/com/stuypulse/robot/commands/intake/IntakeAutoDigest.java
+++ src/main/java/com/stuypulse/robot/commands/intake/IntakeAutoDigest.java
@@ -3,19 +3,17 @@ package com.stuypulse.robot.commands.intake;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 
+/**
+ * Runs the digestion routine 3 times, 0.5sec pause between each state change.
+ */
 public class IntakeAutoDigest extends SequentialCommandGroup {
-
     public IntakeAutoDigest() {
-
         addCommands(
-
-        new IntakeDigest().andThen(new WaitCommand(0.5)).andThen(new IntakeDeploy()).andThen(new WaitCommand(0.5)).andThen(
-            new IntakeDigest().andThen(new WaitCommand(0.5)).andThen(new IntakeDeploy())).andThen(new WaitCommand(0.5)).andThen(
                 new IntakeDigest().andThen(new WaitCommand(0.5)).andThen(new IntakeDeploy())
-            )
-
-        );
-
+                        .andThen(new WaitCommand(0.5))
+                        .andThen(new IntakeDigest().andThen(new WaitCommand(0.5)).andThen(new IntakeDeploy()))
+                        .andThen(new WaitCommand(0.5))
+                        .andThen(new IntakeDigest().andThen(new WaitCommand(0.5)).andThen(new IntakeDeploy()))
+                        .andThen(new WaitCommand(0.5)));
     }
-    
 }
